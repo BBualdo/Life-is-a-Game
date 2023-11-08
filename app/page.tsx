fd3a16e2ec362b1ad4117506f331308ebc5520b1
Sebastian <mrseb10@gmail.com>
2023-11-08 20:09:07 +0100

    Font, background and buttons

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,14 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <>
+      <div
+        aria-hidden
+        className="bg-black fixed top-0 bottom-0 w-full bg-rog bg-no-repeat bg-center min-h-screen"
+      />
+      <div className="flex flex-col items-center min-h-screen justify-center gap-4 backdrop-blur-sm transition-all duration-1000">
+        <button className="btn btn-cyan hover:btn-red">Log in</button>
+        <button className="btn btn-cyan hover:btn-red">Create account</button>
+      </div>
+    </>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,22 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        rog: "url('/assets/images/rog.gif')",
+      },
+      colors: {
+        "cp-cyan": "#00f0ff",
+        "cp-red": "#cd0001",
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
