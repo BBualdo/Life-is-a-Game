bc9ed39c5f34f893c1e4a7ea3d9e3c5b6dfa48c2
Sebastian <mrseb10@gmail.com>
2023-12-03 10:35:45 +0100

    Profile page waiting for data

--- src/components/Interface/UserLevel.tsx
+++ src/components/Interface/UserLevel.tsx
@@ -1,16 +1,18 @@
 const UserLevel = () => {
   return (
-    <div className="flex flex-1 flex-col items-center justify-center px-4">
-      <div className="w-1/2">
-        <div className="flex w-full items-center justify-between text-cp-red">
-          <p>Level: 1</p>
-          <p>XP: 50/100</p>
-        </div>
-        <div className="h-[20px] w-full -skew-x-12 bg-cp-red/30">
-          <div
-            className={`h-full w-[50%] bg-cp-red transition-all duration-300`}
-          />
-        </div>
+    <div className="flex w-full flex-col items-center justify-center px-4">
+      <div className="flex w-full items-end justify-between text-cp-red">
+        <p className="text-2xl">
+          Level: <span className="text-4xl font-bold">1</span>
+        </p>
+        <p className="text-xl">
+          XP: <span className="font-bold">50/100</span>
+        </p>
+      </div>
+      <div className="h-[20px] w-full -skew-x-12 bg-cp-red/30">
+        <div
+          className={`h-full w-[50%] bg-cp-red transition-all duration-300`}
+        />
       </div>
     </div>
   );
