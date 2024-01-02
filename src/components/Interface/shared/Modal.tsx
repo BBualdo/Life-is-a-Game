dab6dcf5d18dfd49a0c49991d4587f32e7c52137
Sebastian <mrseb10@gmail.com>
2024-01-02 21:43:34 +0100

    Modal opening animation

--- src/components/Interface/shared/Modal.tsx
+++ src/components/Interface/shared/Modal.tsx
@@ -3,6 +3,7 @@
 import { useState, useEffect } from "react";
 import { createPortal } from "react-dom";
 import ModalBackdrop from "./ModalBackdrop";
+import { motion } from "framer-motion";
 
 const Modal = ({
   children,
@@ -24,11 +25,22 @@ const Modal = ({
 
   return mounted && isOpen
     ? createPortal(
-        <div className="fixed bottom-0 top-0 z-50 flex w-full items-center justify-center transition-all duration-500">
+        <div className="fixed bottom-0 top-0 z-50 flex w-full items-center justify-center">
           <ModalBackdrop />
-          <div className="gradient-cp-cyan z-50 max-h-[90vh] w-3/4 overflow-y-auto border-2 border-cp-cyan px-10 py-4 shadow-[0_0_12px_#34d0ff]">
-            {children}
-          </div>
+          <motion.div
+            initial={{ width: "0vw" }}
+            animate={{ width: "75vw" }}
+            transition={{ duration: 0.3, ease: "anticipate" }}
+            className="gradient-cp-cyan z-50 max-h-[90vh] overflow-y-auto border-2 border-cp-cyan px-10 py-4 shadow-[0_0_12px_#34d0ff]"
+          >
+            <motion.div
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 100 }}
+              transition={{ duration: 0.5, delay: 0.2 }}
+            >
+              {children}
+            </motion.div>
+          </motion.div>
         </div>,
         document.body,
       )
