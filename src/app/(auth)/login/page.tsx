66f949323b4400a01b62a6f294a99c3d74d9c754
Sebastian <mrseb10@gmail.com>
2024-01-08 23:26:41 +0100

    Fixed deploy error

--- src/app/(auth)/login/page.tsx
+++
@@ -1,7 +0,0 @@
-// import LoginForm from "@/src/components/Auth/LoginForm";
-
-// const Login = () => {
-//   return <LoginForm />;
-// };
-
-// export default Login;
--- src/app/(auth)/signup/page.tsx
+++
@@ -1,7 +0,0 @@
-// import SignupForm from "@/src/components/Auth/SignupForm";
-
-// const Signup = () => {
-//   return <SignupForm />;
-// };
-
-// export default Signup;
