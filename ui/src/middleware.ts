9bbf881e45859d240536505465659438869730f5
Sebastian <mrseb10@gmail.com>
2024-08-06 10:00:06 +0200

    Cookies are empty, that's the issue for sure

--- ui/src/middleware.ts
+++ ui/src/middleware.ts
@@ -1,11 +1,14 @@
 import { NextResponse, type NextRequest } from "next/server";
+import { getToken } from "next-auth/jwt";
 
-export function middleware(req: NextRequest) {
+export async function middleware(req: NextRequest) {
   const requestCookies = req.cookies;
   console.log("Request Cookies: ", requestCookies);
   const response = NextResponse.next();
   const responseCookies = response.cookies;
   console.log("Response Cookies: ", responseCookies);
+  const cookies = await getToken({ req });
+  console.log("Cookies: ", cookies);
   let token = requestCookies.get("LIAGToken")?.value;
   console.log("Token: ", token);
 
