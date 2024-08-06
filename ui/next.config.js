86b0daaa680f12b5180e11383252bbe44b9c9160
Sebastian <mrseb10@gmail.com>
2024-08-06 10:23:48 +0200

    Disposed of middleware and did the same logic with redirects in next.config file

--- ui/next.config.js
+++ ui/next.config.js
@@ -1,14 +1,74 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
+  async redirects() {
+    return [
       {
-        protocol: "https",
-        hostname: "i.ibb.co",
-        port: "",
-        pathname: "/**",
+        source: "/login",
+        has: [
+          {
+            type: "cookie",
+            key: "LIAGToken",
+          },
+        ],
+        destination: "/",
+        permanent: false,
       },
-    ],
+      {
+        source: "/signup",
+        has: [
+          {
+            type: "cookie",
+            key: "LIAGToken",
+          },
+        ],
+        destination: "/",
+        permanent: false,
+      },
+      {
+        source: "/",
+        missing: [
+          {
+            type: "cookie",
+            key: "LIAGToken",
+          },
+        ],
+        destination: "/login",
+        permanent: false,
+      },
+      {
+        source: "/missions",
+        missing: [
+          {
+            type: "cookie",
+            key: "LIAGToken",
+          },
+        ],
+        destination: "/login",
+        permanent: false,
+      },
+      {
+        source: "/achievements",
+        missing: [
+          {
+            type: "cookie",
+            key: "LIAGToken",
+          },
+        ],
+        destination: "/login",
+        permanent: false,
+      },
+      {
+        source: "/profile",
+        missing: [
+          {
+            type: "cookie",
+            key: "LIAGToken",
+          },
+        ],
+        destination: "/login",
+        permanent: false,
+      },
+    ];
   },
   reactStrictMode: false,
 };
